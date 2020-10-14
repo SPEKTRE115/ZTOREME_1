011035b81c915ebf6f12c854fad03c4b0845f27d
Eduardo Aguilar Yáñez <eduardoay115@gmail.com>
2020-10-14 15:46:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ztoreme_1/FirstFragment.kt
@@ -0,0 +1,36 @@
+package com.example.ztoreme_1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+
+
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
