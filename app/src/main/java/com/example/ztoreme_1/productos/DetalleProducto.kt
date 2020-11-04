40b2f89022b7700ec3e5f43af6e8b5e2de623712
Eduardo Aguilar Yáñez <eduardoay115@gmail.com>
2020-11-03 23:25:28 -0600

    Validación de campo vacio en cambiar stock

--- app/src/main/java/com/example/ztoreme_1/productos/DetalleProducto.kt
+++ app/src/main/java/com/example/ztoreme_1/productos/DetalleProducto.kt
@@ -56,74 +56,101 @@ class DetalleProducto : AppCompatActivity() {
 
         btn_borrar.setOnClickListener {
             val txtcantidad = findViewById(R.id.cantidad) as TextView
-            builder.setTitle("Quitar articulos del inventario")
-            builder.setMessage("¿Estas seguro de quitar " + (txtcantidad.text.toString()).toInt() + " artículos del producto: " + producto.nombreProducto + "?")
-            val lp = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            )
+            if(!txtcantidad.text.isNullOrEmpty()) {
+                builder.setTitle("Quitar articulos del inventario")
+                builder.setMessage("¿Estas seguro de quitar " + (txtcantidad.text.toString()).toInt() + " artículos del producto: " + producto.nombreProducto + "?")
+                val lp = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+                )
 
-            builder.setPositiveButton(
-                "Guardar cambios",
-                { dialogInterface: DialogInterface, i: Int ->
-                    val txtcantidad = findViewById(R.id.cantidad) as TextView
-                    var cantidad = (txtcantidad.text.toString()).toInt()
-                    val txtcantidadactual = findViewById(R.id.stock_actual) as TextView
-                    var cantidadactual = (txtcantidadactual.text.toString()).toInt()
-                    //val txtcantidadmax = findViewById(R.id.stock_actual) as TextView
-                    //var cantidadmax = (txtcantidadactual.text.toString()).toInt()
-                    if ((cantidadactual - cantidad)<0) {
-                        Toast.makeText(context, "No puedes quitar más productos de los que tienes", Toast.LENGTH_SHORT).show()
-                    } else {
-                        var db = DataBaseHandler(context)
-                        db.disminuirStockProducto(producto.nombreProducto, cantidad)
-                        Toast.makeText(context, "Producto modificado", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, MisProductos::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                })
+                builder.setPositiveButton(
+                    "Guardar cambios",
+                    { dialogInterface: DialogInterface, i: Int ->
+                        val txtcantidad = findViewById(R.id.cantidad) as TextView
+                        if (!txtcantidad.text.isNullOrEmpty()) {
+                            var cantidad = (txtcantidad.text.toString()).toInt()
+                            val txtcantidadactual = findViewById(R.id.stock_actual) as TextView
+                            var cantidadactual = (txtcantidadactual.text.toString()).toInt()
+                            //val txtcantidadmax = findViewById(R.id.stock_actual) as TextView
+                            //var cantidadmax = (txtcantidadactual.text.toString()).toInt()
+                            if ((cantidadactual - cantidad) < 0) {
+                                Toast.makeText(
+                                    context,
+                                    "No puedes quitar más productos de los que tienes",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } else {
+                                var db = DataBaseHandler(context)
+                                db.disminuirStockProducto(producto.nombreProducto, cantidad)
+                                Toast.makeText(context, "Producto modificado", Toast.LENGTH_SHORT)
+                                    .show()
+                                val intent = Intent(this, MisProductos::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                        } else {
+                            Toast.makeText(context, "Ingresa una cantidad", Toast.LENGTH_SHORT)
+                                .show()
+                            finish()
+                        }
+                    })
 
-            builder.setNegativeButton("Cancelar", { dialogInterface: DialogInterface, i: Int -> })
-            builder.show()
+                builder.setNegativeButton(
+                    "Cancelar",
+                    { dialogInterface: DialogInterface, i: Int -> })
+                builder.show()
+            }else{
+                Toast.makeText(context, "Ingresa una cantidad", Toast.LENGTH_SHORT).show()
+            }
+
 
         }
 
         btn_agregar.setOnClickListener {
             val txtcantidad = findViewById(R.id.cantidad) as TextView
             builder.setTitle("Agregar articulos al inventario")
-            builder.setMessage("¿Estas seguro de agregar "+(txtcantidad.text.toString()).toInt()+" artículos del producto: " + producto.nombreProducto+"?")
+            if(!txtcantidad.text.isNullOrEmpty()) {
+                builder.setMessage("¿Estas seguro de agregar " + (txtcantidad.text.toString()).toInt() + " artículos del producto: " + producto.nombreProducto + "?")
 
-            val lp = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-            )
+                val lp = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+                )
 
-            builder.setPositiveButton(
-                "Guardar cambios"
-            ) { dialogInterface: DialogInterface, i: Int ->
-                val txtcantidad = findViewById(R.id.cantidad) as TextView
-                var cantidad = (txtcantidad.text.toString()).toInt()
-                val txtcantidadactual = findViewById(R.id.stock_actual) as TextView
-                var cantidadactual = (txtcantidadactual.text.toString()).toInt()
-                val txtcantidadmax = findViewById(R.id.stock_maximo) as TextView
-                var cantidadmax = (txtcantidadmax.text.toString()).toInt()
+                builder.setPositiveButton(
+                    "Guardar cambios"
+                ) { dialogInterface: DialogInterface, i: Int ->
+                    val txtcantidad = findViewById(R.id.cantidad) as TextView
+                    var cantidad = (txtcantidad.text.toString()).toInt()
+                    val txtcantidadactual = findViewById(R.id.stock_actual) as TextView
+                    var cantidadactual = (txtcantidadactual.text.toString()).toInt()
+                    val txtcantidadmax = findViewById(R.id.stock_maximo) as TextView
+                    var cantidadmax = (txtcantidadmax.text.toString()).toInt()
 
-                if(cantidadactual+cantidad>cantidadmax){
-                    Toast.makeText(context, "No puedes pasar tu stock máximo", Toast.LENGTH_SHORT).show()
-                }else{
-                    var db = DataBaseHandler(context)
-                    db.aumentarStockProducto(producto.nombreProducto, cantidad)
-                    Toast.makeText(context, "Producto modificado", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, MisProductos::class.java)
-                    startActivity(intent)
-                    finish()
+                    if (cantidadactual + cantidad > cantidadmax) {
+                        Toast.makeText(
+                            context,
+                            "No puedes pasar tu stock máximo",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        var db = DataBaseHandler(context)
+                        db.aumentarStockProducto(producto.nombreProducto, cantidad)
+                        Toast.makeText(context, "Producto modificado", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MisProductos::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
                 }
 
-            }
+                builder.setNegativeButton("Cancelar", { dialogInterface: DialogInterface, i: Int -> })
+                builder.show()
 
-            builder.setNegativeButton("Cancelar", { dialogInterface: DialogInterface, i: Int -> })
-            builder.show()
+            }else{
+                Toast.makeText(context, "Ingresa una cantidad", Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
